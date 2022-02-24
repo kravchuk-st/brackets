da1ef42b06854bd7c406e75da28f960625c6a687
kravchuk-st <57140996+kravchuk-st@users.noreply.github.com>
2022-02-24 22:17:02 +0200

    feat: add js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const matrix = Object.fromEntries(bracketsConfig);
+  let stack = [str[0]];
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === matrix[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+
+  return stack.length ? false : true;
 }
